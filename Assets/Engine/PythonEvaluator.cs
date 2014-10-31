a7aa32932c35a2420a9c27d6f2b2b0922eb88b7d
Michael Kirschner <mjk.kirschner@gmail.com>
2014-10-31 02:23:19 -0400

    adding initial algorithms for graph traversal using a stack

---
+++ Assets/Engine/PythonEvaluator.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Nodeplay.Interfaces;
+using System.ComponentModel;
+
+using IronPython;
+using IronPython.Modules;
+using System.Text;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.IO;
+
+
+namespace Nodeplay.Engine
+{
+    class PythonEvaluator : MonoBehaviour
+    {
+
+        public String code;
+        public List<String> names;
+        public List<System.Object> vals;
+        public String StdOut;
+
+        public void Start()
+        {
+            names = new List<String>() { "name1", "name2" };
+            vals = new List<System.Object>() { 1, 2 };
+            Debug.Log(Evaluate(code, names, vals));
+
+        }
+
+        public System.Object Evaluate(string script, List<string> variableNames, List<System.Object> variableValues)
+        {
+
+            var engine = IronPython.Hosting.Python.CreateEngine();
+
+            var scope = engine.CreateScope();
+            System.Object output = null;
+
+
+            foreach (var variable in variableNames)
+            {
+                var index = variableNames.IndexOf(variable);
+                // do we need to do some conversion of this type...TODO
+                scope.SetVariable(variable, variableValues[index]);
+                Debug.Log("setting" + variable + "to" + variableValues[index].ToString());
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                engine.Runtime.IO.SetOutput(memoryStream, new StreamWriter(memoryStream));
+                try
+                {
+
+                    engine.CreateScriptSourceFromString(script).Execute(scope);
+                }
+                catch (Exception e)
+                {
+
+                    string error = engine.GetService<ExceptionOperations>().FormatException(e);
+                    Debug.LogException(e);
+                }
+                finally
+                {
+
+                    var length = (int)memoryStream.Length;
+                    var bytes = new byte[length];
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    memoryStream.Read(bytes, 0, length);
+                    StdOut = Encoding.UTF8.GetString(bytes, 0, length).Trim();
+
+
+                }
+                if (scope.ContainsVariable("OUTPUT"))
+                {
+                    output = scope.GetVariable("OUTPUT");
+                }
+                else
+                {
+                    output = "No OUTPUT variable was defined in the python code";
+                }
+
+                return output;
+            }
+
+
+        }
+
+    }
+}
+		 
+
