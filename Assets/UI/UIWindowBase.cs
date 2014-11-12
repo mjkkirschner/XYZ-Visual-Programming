a1360f2ad4b668c5cd10a51846d54714d6523bbd
Michael Kirschner <mjk.kirschner@gmail.com>
2014-11-12 09:12:47 -0500

    only made one change - using the cached renderer in selectable mesh renderer but unfortunately mlnodevelop shifted all lines

--- Assets/UI/UIWindowBase.cs
+++ Assets/UI/UIWindowBase.cs
@@ -6,35 +6,34 @@ using System.Linq;
 
 public class UIWindowBase : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
-    RectTransform m_transform = null;
-    float dist = 100;
-    // Use this for initialization
-    void Start()
-    {
-        m_transform = GetComponent<RectTransform>();
+		RectTransform m_transform = null;
+		float dist = 100;
+		// Use this for initialization
+		void Start ()
+		{
+				m_transform = GetComponent<RectTransform> ();
         
-    }
+		}
 
 
-    public void OnPointerDown(PointerEventData pointerdata)
-    {
-        dist = Vector3.Distance(m_transform.position, Camera.main.transform.position);
-    }
+		public void OnPointerDown (PointerEventData pointerdata)
+		{
+				dist = Vector3.Distance (m_transform.position, Camera.main.transform.position);
+		}
 
-    public void OnDrag(PointerEventData eventData)
-    {
+		public void OnDrag (PointerEventData eventData)
+		{
        
 
-        //need to construct a 3d vector that we actually move the window along
-        // can do this from the point on the object we dragged to, and the 
+				//need to construct a 3d vector that we actually move the window along
+				// can do this from the point on the object we dragged to, and the 
 
-        if (this.gameObject == eventData.pointerDrag)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(eventData.position - eventData.delta);
-            var orginalPoint = ray.GetPoint(dist);
-            Vector3 threeddelta =  BaseView<BaseModel>.ProjectCurrentDrag(dist) - orginalPoint;
-            m_transform.position += threeddelta;
-        }
-        // magic : add zone clamping if's here.
-    }
+				if (this.gameObject == eventData.pointerDrag) {
+						Ray ray = Camera.main.ScreenPointToRay (eventData.position - eventData.delta);
+						var orginalPoint = ray.GetPoint (dist);
+						Vector3 threeddelta = BaseView<BaseModel>.ProjectCurrentDrag (dist) - orginalPoint;
+						m_transform.position += threeddelta;
+				}
+				// magic : add zone clamping if's here.
+		}
 }
