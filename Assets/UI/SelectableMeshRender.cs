8ffc9abec449209dd9172e062b7e459a63f55871
Michael Kirschner <mjk.kirschner@gmail.com>
2014-11-12 09:12:47 -0500

    only made one change - using the cached renderer in selectable mesh renderer but unfortunately mlnodevelop shifted all lines

--- Assets/UI/SelectableMeshRender.cs
+++ Assets/UI/SelectableMeshRender.cs
@@ -11,61 +11,54 @@ using System.Collections;
 namespace Nodeplay.UI
 {   //TODO may push this into BaseView, 
     
-    class SelectableMeshRender:Selectable
-    {
+		class SelectableMeshRender:Selectable
+		{
 
-        public Renderer obUI;
+				public Renderer obUI;
         
        
-            protected override void OnEnable()
-        {
-            obUI = this.GetComponentInChildren<Renderer>();
-            base.OnEnable();
+				protected override void OnEnable ()
+				{
+						obUI = this.GetComponentInChildren<Renderer> ();
+						base.OnEnable ();
         
         
-        }
+				}
 
 
-        
+				protected virtual IEnumerator TweenColorFromCurrent (Color ToColor, float duration)
+				{
+						for (float f = 0; f <= duration; f = f + Time.deltaTime) {
 
-       
-        protected virtual IEnumerator TweenColorFromCurrent(Color ToColor, float duration)
-        {
-            for (float f = 0; f <= duration; f = f + Time.deltaTime)
-            {
+								obUI.material.color = Color.Lerp (renderer.material.color, ToColor, f);
+								yield return null;
 
-                renderer.material.color = Color.Lerp(renderer.material.color, ToColor, f);
-                yield return null;
-
-            }
-            renderer.material.color = ToColor;
-        }
+						}
+						obUI.material.color = ToColor;
+				}
 
 
-        protected override void DoStateTransition(Selectable.SelectionState state, bool instant)
-        {
+				protected override void DoStateTransition (Selectable.SelectionState state, bool instant)
+				{
            
-            Debug.Log("inside state transition");
+						Debug.Log ("inside state transition");
 
-            if (state == SelectionState.Pressed)
-            {
-                StopAllCoroutines();
-                StartCoroutine(TweenColorFromCurrent(this.colors.pressedColor, this.colors.fadeDuration));
-            }
+						if (state == SelectionState.Pressed) {
+								StopAllCoroutines ();
+								StartCoroutine (TweenColorFromCurrent (this.colors.pressedColor, this.colors.fadeDuration));
+						}
            
-                if (state == Selectable.SelectionState.Highlighted)
-                {
-                    Debug.Log("state was highlight");
-                    //StopAllCoroutines();
-                    StartCoroutine(TweenColorFromCurrent(this.colors.highlightedColor, this.colors.fadeDuration));
-                }
-                if (state == Selectable.SelectionState.Normal)
-                {
-                    Debug.Log("state was normal");
-                    StopAllCoroutines();
-                    StartCoroutine(TweenColorFromCurrent(this.colors.normalColor, this.colors.fadeDuration));
-                }
+						if (state == Selectable.SelectionState.Highlighted) {
+								Debug.Log ("state was highlight");
+								//StopAllCoroutines();
+								StartCoroutine (TweenColorFromCurrent (this.colors.highlightedColor, this.colors.fadeDuration));
+						}
+						if (state == Selectable.SelectionState.Normal) {
+								Debug.Log ("state was normal");
+								StopAllCoroutines ();
+								StartCoroutine (TweenColorFromCurrent (this.colors.normalColor, this.colors.fadeDuration));
+						}
             
-        }
-    }
+				}
+		}
 }
