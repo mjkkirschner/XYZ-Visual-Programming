d67b3dc6ddf48e795c046585e4bc6e136c784cad
Michael Kirschner <mjk.kirschner@gmail.com>
2014-12-07 17:11:03 -0500

    added a red markdown tag for operators

--- Assets/UI/ExpandingInputField.cs
+++ Assets/UI/ExpandingInputField.cs
@@ -8,59 +8,71 @@ using System.Text.RegularExpressions;
 namespace Nodeplay.UI
 {
 
-    /// <summary>
-    /// behavior to do horrible hack to input field, will force input field
-    /// to expand by resizing the background image to the unbounded rect of
-    /// it's full text
-    /// </summary>
-    /// 
-    public class ExpandingInputField : UIBehaviour
-    {
-        public GameObject inputparent;
-        InputField inf;
+	/// <summary>
+	/// behavior to do horrible hack to input field, will force input field
+	/// to expand by resizing the background image to the unbounded rect of
+	/// it's full text
+	/// </summary>
+	/// 
+	public class ExpandingInputField : UIBehaviour
+	{
+		public GameObject inputparent;
+		InputField inf;
 		private Regex colorTags = new Regex("<[^>]*>");
-		private Regex keyWordsBlue = new Regex("if |then |else |fi |true |while |do |done |set |export |bool |break |case |class |const |for |foreach |goto |in |void |if\n|then\n|else\n|fi\n|true\n|while\n|do\n|done\n|set\n|export\n|bool\n|break\n|case\n|class\n|const\n|for\n|foreach\n|goto\n|in\n|void\n");
-		
+		private Regex keyWords = new Regex("and |assert |break |class |continue |def |del |elif |else |except |exec |finally |for |from |global |if |import |in |is |lambda |not |or |pass |print |raise |return |try |while |yield |None |True |False ");
+		private Regex operators = new Regex("<=|>=|!=|=|>|<");
+		public Regex definedTriggers { get; set; }
 
-        protected override void Start()
-        {
+		protected override void Start()
+		{
 
-            inf = this.gameObject.GetComponent<InputField>();
-            inf.onValueChange.AddListener(new UnityEngine.Events.UnityAction<string>(ResizeInput));
+			inf = this.gameObject.GetComponent<InputField>();
+			inf.onValueChange.AddListener(new UnityEngine.Events.UnityAction<string>(ResizeInput));
 			inf.onEndEdit.AddListener(new UnityEngine.Events.UnityAction<string>(highlight));
-			
-        }
-
-
-		public void highlight(string text){
-			
-				inf.text = colorTags.Replace(inf.text, @"");
-				inf.text = keyWordsBlue.Replace(inf.text, @"<color=blue>$&</color>");
-				inf.MoveTextEnd(false);
-			
-			
+			inf.onValueChange.AddListener(new UnityEngine.Events.UnityAction<string>(removeTags));
 		}
 
-        void ResizeInput(string text)
-        {
 
-			
-            Debug.Log("some kind of resizing horror");
-            var fullText = inf.text;
+		public void highlight(string text)
+		{
 
-            Vector2 extents = inf.textComponent.rectTransform.rect.size;
-            var settings = inf.textComponent.GetGenerationSettings(extents);
-            settings.generateOutOfBounds = false;
-            var prefheight = new TextGenerator().GetPreferredHeight(fullText, settings) + 10;
+			inf.text = colorTags.Replace(inf.text, @"");
+			inf.text = keyWords.Replace(inf.text, @"<color=blue>$&</color>");
+			inf.text = operators.Replace(inf.text, @"<color=red>$&</color>");
+			if (definedTriggers != null)
+			{
+				inf.text = definedTriggers.Replace(inf.text, @"<color=green>$&</color>");
+			}
+			inf.MoveTextEnd(false);
+		}
 
-            if (prefheight > inf.textComponent.rectTransform.rect.height - 10)
-            {
-                Debug.Log("i grew because the inputfield was only this big" + inf.GetComponent<RectTransform>().rect.height + "and I needed" + prefheight + "space");
-                inputparent.GetComponent<LayoutElement>().preferredHeight = prefheight;
-            }
+		private void removeTags(string text)
+		{
 
-        }
+			inf.text = colorTags.Replace(inf.text, @"");
+			inf.MoveTextEnd(false);
+		}
 
-    }
+		void ResizeInput(string text)
+		{
+
+
+			Debug.Log("some kind of resizing horror");
+			var fullText = inf.text;
+
+			Vector2 extents = inf.textComponent.rectTransform.rect.size;
+			var settings = inf.textComponent.GetGenerationSettings(extents);
+			settings.generateOutOfBounds = false;
+			var prefheight = new TextGenerator().GetPreferredHeight(fullText, settings) + 10;
+
+			if (prefheight > inf.textComponent.rectTransform.rect.height - 10)
+			{
+				Debug.Log("i grew because the inputfield was only this big" + inf.GetComponent<RectTransform>().rect.height + "and I needed" + prefheight + "space");
+				inputparent.GetComponent<LayoutElement>().preferredHeight = prefheight;
+			}
+
+		}
+
+	}
 
 }
